429beec857b920caa6430859d10f6e620d11ed0b
Hang Chen <chen_h@outlook.com>
2022-06-23 01:04:03 -0500

    Update rm-perm.sh

--- src/rm-perm.sh
+++ src/rm-perm.sh
@@ -5,9 +5,9 @@ read -n1 -s -r -p "$(echo -e $TEXT_YELLOW' \nWARNING! Remove all trashed files p
 case "$choice" in
   y|Y )
   rm -rf ~/.trash/*
-  echo -e " \n${TEXT_YELLOW}Files deleted permanently.${TEXT_RESET} \n";;
+  echo -e " \n${TEXT_YELLOW}Files deleted permanently. ${TEXT_RESET} \n";;
   n|N )
-  echo -e " \n${TEXT_YELLOW}Deletion cancelled.${TEXT_RESET} \n";;
+  echo -e " \n${TEXT_YELLOW}Deletion cancelled. ${TEXT_RESET} \n";;
   * )
-  echo -e " \n${TEXT_YELLOW}Deletion cancelled.${TEXT_RESET} \n";;
+  echo -e " \n${TEXT_YELLOW}Deletion cancelled. ${TEXT_RESET} \n";;
 esac
