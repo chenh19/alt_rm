0f104e2e1f400c82def50639a5a81da799d558b2
Hang Chen <chen_h@outlook.com>
2022-06-22 19:24:14 -0500

    Add files via upload

---
+++ uninstall.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# set terminal font color
+TEXT_YELLOW='\e[1;33m'
+TEXT_GREEN='\e[1;32m'
+TEXT_RESET='\e[0m'
+
+# confirm removal
+read -n1 -s -r -p "$(echo -e $TEXT_YELLOW'Remove alternative <rm> command? [y/n]'$TEXT_RESET)"$' \n' choice
+case "$choice" in
+  y|Y ) # notify start
+        echo -e " \n${TEXT_YELLOW}Uninstalling alternative <rm> command... ${TEXT_RESET} \n" && sleep 1
+
+        # remove alias
+        sed -i "s+alias rm='bash ~/.rm.sh'++g" ~/.bashrc
+        sed -i "s+alias rm-clean='bash ~/.rm-clean.sh'++g" ~/.bashrc
+        sleep 1
+
+        # remove shell scripts
+        rm ~/.rm.sh ~/.rm-perm.sh && sleep 1
+
+        # notify end
+        echo -e "${TEXT_GREEN}Alternative <rm> command removed! Please reopen the terminal. ${TEXT_RESET} \n" && sleep 1;;
+
+  n|N ) # notify end
+        echo -e "${TEXT_GREEN}Alternative <rm> command not removed. ${TEXT_RESET} \n" && sleep 1;;
+
+  * )   # notify end
+        echo -e "${TEXT_GREEN}Alternative <rm> command not removed. ${TEXT_RESET} \n" && sleep 1;;
+esac
+echo -e "${TEXT_GREEN}All trashed files remain in ~/.trash/ folder. ${TEXT_RESET} \n" && sleep 1;;
