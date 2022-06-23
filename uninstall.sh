7e25f3cfacecda6d715da5b06d16df506a6cf8fa
Hang Chen <chen_h@outlook.com>
2022-06-22 20:05:08 -0500

    Update uninstall.sh

--- uninstall.sh
+++ uninstall.sh
@@ -23,10 +23,10 @@ case "$choice" in
         echo -e "${TEXT_GREEN}Alternative <rm> command removed! Please reopen the terminal. ${TEXT_RESET} \n" && sleep 1;;
 
   n|N ) # notify end
-        echo -e "${TEXT_GREEN}Alternative <rm> command not removed. ${TEXT_RESET} \n" && sleep 1;;
+        echo -e " \n${TEXT_GREEN}Alternative <rm> command not removed. ${TEXT_RESET} \n" && sleep 1;;
 
   * )   # notify end
-        echo -e "${TEXT_GREEN}Alternative <rm> command not removed. ${TEXT_RESET} \n" && sleep 1;;
+        echo -e " \n${TEXT_GREEN}Alternative <rm> command not removed. ${TEXT_RESET} \n" && sleep 1;;
 esac
 
 # notify trash files path
