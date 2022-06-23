bb1f406cba31b1d516a38ac203b87feb8051d2a9
Hang Chen <chen_h@outlook.com>
2022-06-22 21:21:18 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -9,8 +9,8 @@ TEXT_RESET='\e[0m'
 echo -e " \n${TEXT_YELLOW}Installing alternative <rm> command... ${TEXT_RESET} \n" && sleep 1
 
 # deploy shell scripts
-cd ~
-[ ! -d ~/.trash/ ] && mkdir ~/.trash/ && cd ~/.trash/ && sleep 1
+[ ! -d ~/.trash/ ] && mkdir ~/.trash/
+cd ~/.trash/
 [ ! -f alt_rm.zip ] && wget -q https://codeload.github.com/chenh19/alt_rm/zip/refs/heads/main -O alt_rm.zip && sleep 1
 unzip -o -q alt_rm.zip && rm alt_rm.zip && sleep 1
 mv -f ./alt_rm-main/src/rm.sh ~/.rm.sh && mv -f ./alt_rm-main/src/rm-perm.sh ~/.rm-perm.sh && rm -rf ./alt_rm-main/ && sleep 1
