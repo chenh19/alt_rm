088fef591c8e54950fc5bcbc1fae3457ac91f0ec
Hang Chen <chen_h@outlook.com>
2022-06-22 22:56:53 -0500

    Update rm-perm.sh

--- src/rm-perm.sh
+++ src/rm-perm.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 TEXT_YELLOW='\e[1;33m'
 TEXT_RESET='\e[0m'
-read -n1 -s -r -p "$(echo -e $TEXT_YELLOW' \nWARNING! Remove all trashed files permenently? [y/n]'$TEXT_RESET)"$' \n' choice
+read -n1 -s -r -p "$(echo -e $TEXT_YELLOW' \nWARNING! Remove all trashed files permanently? [y/n]'$TEXT_RESET)"$' \n' choice
 case "$choice" in
   y|Y )
   rm -rf ~/.trash/*
