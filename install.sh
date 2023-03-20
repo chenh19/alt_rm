899c986398d642e90b96952c58b40f617e9f6aea
Hang Chen <chen_h@outlook.com>
2023-03-20 13:31:37 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -13,13 +13,13 @@ echo -e " \n${TEXT_YELLOW}Configuring alternative <rm> command... ${TEXT_RESET} 
 cd ~/.trash/
 [ ! -f alt_rm.zip ] && wget -q https://codeload.github.com/chenh19/alt_rm/zip/refs/heads/main -O alt_rm.zip && sleep 1
 unzip -o -q alt_rm.zip && rm alt_rm.zip && sleep 1
-mv -f ./alt_rm-main/src/rm.sh ~/.rm.sh && mv -f ./alt_rm-main/src/rm-check.sh ~/.rm-check.sh && mv -f ./alt_rm-main/src/rm-perm.sh ~/.rm-perm.sh && rm -rf ./alt_rm-main/ && sleep 1
+mv -f ./alt_rm-main/src/rm.sh ~/.rm.sh && mv -f ./alt_rm-main/src/rm-check.sh ~/.rm-check.sh && mv -f ./alt_rm-main/src/rm-purge.sh ~/.rm-purge.sh && rm -rf ./alt_rm-main/ && sleep 1
 
 # set alias in bash
 [ ! -f ~/.bashrc] ] && touch ~/.bashrc
 if ! grep -q "alias rm='bash ~/.rm.sh >/dev/null 2>&1'" ~/.bashrc ; then echo -e "\nalias rm='bash ~/.rm.sh >/dev/null 2>&1'" >> ~/.bashrc ; fi
 if ! grep -q "alias rm-check='bash ~/.rm-check.sh'" ~/.bashrc ; then echo -e "alias rm-check='bash ~/.rm-check.sh'" >> ~/.bashrc ; fi
-if ! grep -q "alias rm-perm='bash ~/.rm-perm.sh'" ~/.bashrc ; then echo -e "alias rm-perm='bash ~/.rm-perm.sh'" >> ~/.bashrc ; fi
+if ! grep -q "alias rm-purge='bash ~/.rm-purge.sh'" ~/.bashrc ; then echo -e "alias rm-purge='bash ~/.rm-purge.sh'" >> ~/.bashrc ; fi
 
 # notify end
 echo -e "${TEXT_GREEN}Alternative ${TEXT_YELLOW}<rm>${TEXT_GREEN} command configured! Please reopen terminal before using the command. ${TEXT_RESET} \n" && sleep 1
