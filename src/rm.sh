5af82c9f1fefb3d4c50a93fa0a513d41e249834d
Hang Chen <chen_h@outlook.com>
2024-11-22 11:26:54 -0600

    Update rm.sh

--- src/rm.sh
+++ src/rm.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
-[ ! -d ~/.trash/ ] && mkdir ~/.trash/
-for a; do
-   shift
-   case $a in
-   -*) opts+=("$a");;
-   *) set -- "$@" "$a";;
-   esac
+[ ! -d "$HOME/.trash/" ] && mkdir -p "$HOME/.trash/"
+if [ $# -eq 0 ]; then echo "rm: missing operand" >&2 && exit 1; fi
+for file in "$@"; do
+    [[ "$file" == -* ]] && continue
+    if [ -e "$file" ]; then
+        trash="$HOME/.trash/$(basename "$file")"
+        counter=1
+        while [ -e "$trash" ]; do trash="$HOME/.trash/$(basename "$file").~$((++counter))~"; done
+        mv -f "$file" "$trash"
+    else
+        echo "rm: cannot remove '$file': No such file or directory" >&2
+    fi
 done
-mv -f "${@%$'\n'}" ~/.trash/
-#mv -f --backup=numbered "${@%$'\n'}" ~/.trash/
