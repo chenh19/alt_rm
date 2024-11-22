abcf83c4a18c7001118ff9c8b394debbc3fc9193
Hang Chen <chen_h@outlook.com>
2024-11-22 09:49:28 -0600

    Update rm_universal.sh

--- src/rm_universal.sh
+++ src/rm_universal.sh
@@ -2,13 +2,11 @@
 TRASH_DIR="${HOME}/.trash"
 [ ! -d "$TRASH_DIR" ] && mkdir -p "$TRASH_DIR"
 for file in "$@"; do
+    [[ "$file" == -* ]] && continue
     if [ -e "$file" ]; then
-        base="${TRASH_DIR}/$(basename "$file")"
-        counter=1
-        while [ -e "$base.~$counter~" ]; do
-            ((counter++))
-        done
-        mv -f "$file" "${base}${counter:+.~$counter~}"
+        dest="${TRASH_DIR}/$(basename "$file")"
+        while [ -e "$dest" ]; do dest="${TRASH_DIR}/$(basename "$file").~$((++counter))~"; done
+        mv -f "$file" "$dest"
     else
         echo "Warning: $file not found" >&2
     fi
