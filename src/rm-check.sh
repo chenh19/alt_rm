b54bca8be26539b012ec18e836b68ffd393cc99d
Hang Chen <chen_h@outlook.com>
2024-11-22 13:23:53 -0600

    Update rm-check.sh

--- src/rm-check.sh
+++ src/rm-check.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-TEXT_YELLOW='\e[1;33m'
-TEXT_RESET='\e[0m'
+TEXT_YELLOW=$(tput bold; tput setaf 3)
+TEXT_RESET=$(tput sgr0)
 echo -e " \n${TEXT_YELLOW}These are the trashed files: ${TEXT_RESET} \n"
 ls ~/.trash/
 echo ""
