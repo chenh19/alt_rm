c7725756af2dfac580d9993fc0b3602e4137df12
Hang Chen <chen_h@outlook.com>
2024-11-22 10:23:40 -0600

    Update rm_universal.sh

--- src/rm_universal.sh
+++ src/rm_universal.sh
@@ -3,9 +3,9 @@
 for file in "$@"; do
     [[ "$file" == -* ]] && continue
     if [ -e "$file" ]; then
-        dest="$HOME/.trash/$(basename "$file")"
-        while [ -e "$dest" ]; do dest="$HOME/.trash/$(basename "$file").~$((++counter))~"; done
-        mv -f "$file" "$dest"
+        trash="$HOME/.trash/$(basename "$file")"
+        while [ -e "$trash" ]; do trash="$HOME/.trash/$(basename "$file").~$((++counter))~"; done
+        mv -f "$file" "$trash"
     else
         echo "Warning: $file not found" >&2
     fi
