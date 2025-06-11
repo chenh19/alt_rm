31b9acdf6eac7bf2fdaa3c4247630faaf99540f5
Hang Chen <chen_h@outlook.com>
2025-06-11 15:19:44 -0500

    Update rm-check.sh

--- src/rm-check.sh
+++ src/rm-check.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 TEXT_YELLOW=$(tput bold; tput setaf 3)
 TEXT_RESET=$(tput sgr0)
-echo -e " \n${TEXT_YELLOW}These are the trashed files: ${TEXT_RESET} \n"
+echo -e "\n${TEXT_YELLOW}These are the trashed files: ${TEXT_RESET}\n"
 ls ~/.trash/
 echo ""
 kwriteconfig5 --file ~/.trash/.directory --group "Desktop Entry" --key Icon "folder-trash"
