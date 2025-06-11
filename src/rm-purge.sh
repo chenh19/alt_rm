9439cb43bf0bf120e1104148abff4e44a85144cd
Hang Chen <chen_h@outlook.com>
2025-06-11 15:09:15 -0500

    Update rm-purge.sh

--- src/rm-purge.sh
+++ src/rm-purge.sh
@@ -4,7 +4,9 @@ TEXT_RESET=$(tput sgr0)
 read -n1 -s -r -p "$(echo -e $TEXT_YELLOW' \nWARNING! Remove all trashed files permanently? [y/n]'$TEXT_RESET)"$' \n' choice
 case "$choice" in
   y|Y )
+  shopt -s nullglob
   rm -rf ~/.trash/*
+  shopt -u nullglob
   echo -e " \n${TEXT_YELLOW}Files deleted permanently. ${TEXT_RESET} \n";;
   n|N )
   echo -e " \n${TEXT_YELLOW}Deletion cancelled. ${TEXT_RESET} \n";;
