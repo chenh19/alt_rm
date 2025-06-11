223ecdb977575a7301e62dd25d2338d23e971517
Hang Chen <chen_h@outlook.com>
2025-06-11 15:19:29 -0500

    Update rm-purge.sh

--- src/rm-purge.sh
+++ src/rm-purge.sh
@@ -1,16 +1,14 @@
 #!/bin/bash
 TEXT_YELLOW=$(tput bold; tput setaf 3)
 TEXT_RESET=$(tput sgr0)
-read -n1 -s -r -p "$(echo -e $TEXT_YELLOW' \nWARNING! Remove all trashed files permanently? [y/n]'$TEXT_RESET)"$' \n' choice
+read -n1 -s -r -p "$(echo -e $TEXT_YELLOW'\nWARNING! Remove all trashed files permanently? [y/n]'$TEXT_RESET)"$'\n' choice
 case "$choice" in
   y|Y )
   shopt -s nullglob
   rm -rf ~/.trash/*
   shopt -u nullglob
-  echo -e " \n${TEXT_YELLOW}Files deleted permanently. ${TEXT_RESET} \n";;
-  n|N )
-  echo -e " \n${TEXT_YELLOW}Deletion cancelled. ${TEXT_RESET} \n";;
+  echo -e "\n${TEXT_YELLOW}Files deleted permanently. ${TEXT_RESET}\n";;
   * )
-  echo -e " \n${TEXT_YELLOW}Deletion cancelled. ${TEXT_RESET} \n";;
+  echo -e "\n${TEXT_YELLOW}Deletion cancelled. ${TEXT_RESET}\n";;
 esac
 kwriteconfig5 --file ~/.trash/.directory --group "Desktop Entry" --key Icon "folder-trash"
